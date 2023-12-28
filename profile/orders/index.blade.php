59f22c0fcc310fc016471e71f49dd915aa6b82b6
Fady Mondy <EngFadyMondy@gmail.com>
2023-12-28 17:50:23 +0200

    fix bugs

--- profile/orders/index.blade.php
+++ profile/orders/index.blade.php
@@ -20,18 +20,13 @@
             </x-splade-cell>
             <x-splade-cell actions>
                 <div class="flex justify-start gap-2">
-                    @if($item->status !== setting('ordering_cancelled_status'))
-                    <x-tomato-admin-tooltip text="{{__('Cancel')}}">
-                        <x-tomato-admin-button danger type="icon" confirm method="POST" :href="route('profile.orders.cancel', $item->id)">
-                            <x-heroicon-s-x-circle class="h-6 w-6"/>
-                        </x-tomato-admin-button>
-                    </x-tomato-admin-tooltip>
+                    @if($item->status !== setting('ordering_cancelled_status') && $item->status === setting('ordering_pending_status'))
+                        <x-tomato-admin-tooltip text="{{__('Cancel')}}">
+                            <x-tomato-admin-button danger type="icon" confirm method="POST" :href="route('profile.orders.cancel', $item->id)">
+                                <x-heroicon-s-x-circle class="h-6 w-6"/>
+                            </x-tomato-admin-button>
+                        </x-tomato-admin-tooltip>
                     @endif
-{{--                    <x-tomato-admin-tooltip text="{{__('Print')}}">--}}
-{{--                        <x-tomato-admin-button warning type="icon" confirm method="POST" :href="route('profile.orders.print', $item->id)">--}}
-{{--                            <x-heroicon-s-printer class="h-6 w-6"/>--}}
-{{--                        </x-tomato-admin-button>--}}
-{{--                    </x-tomato-admin-tooltip>--}}
                     <x-tomato-admin-tooltip text="{{trans('tomato-admin::global.crud.view')}}">
                         <x-tomato-admin-button success modal type="icon" :href="route('profile.orders.show', $item->id)">
                             <x-heroicon-s-eye class="h-6 w-6"/>
